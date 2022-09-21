805f59cf2b9858e825141cfcf23f64e16f05d3da
Vinicius Lehmann <viniciuslehmann@gmail.com>
2022-09-21 09:59:57 -0300

    Remove KEYS

--- terraform-basics/vars.tf
+++ terraform-basics/vars.tf
@@ -1,17 +1,12 @@
-variable "AWS_ACCESS_KEY" {
-    default = "AKIAVJMDSDADZI5NOQEN"
-}
-
-variable "AWS_SECRET_KEY" {
-    default = "jo+tKlcFlkvAiGCvXJMnR+mFEajQcKtCglgv2Fzw"
-}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
     default = "us-east-1"  
 }
 
 variable "AMIS" {
-    type = "map"
+    type = map
     default = {
         us-east-1 = "ami-08c40ec9ead489470"
         us-west-2 = "ami-017fecd1353bcc96e"
