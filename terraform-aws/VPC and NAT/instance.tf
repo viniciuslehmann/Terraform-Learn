1817c2f0c0450b6c7d9ec725818c44d0d99ea38f
Vinicius Lehmann <viniciuslehmann@gmail.com>
2022-09-26 08:53:11 -0300

    Adicionando Volume EBS Extra

--- terraform-aws/VPC and NAT/instance.tf
+++ terraform-aws/VPC and NAT/instance.tf
@@ -13,3 +13,20 @@ resource "aws_instance" "example" {
   # Chave pública de acesso à instância
   key_name = aws_key_pair.mykey.key_name
 }
+
+# Criação de um volume EBS
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "${var.AWS_REGION}a"
+  size = 20
+  type = "gp2"
+  tags = {
+    "Name" = "extra volume data"
+  }
+} 
+
+# Acoplar volume à uma instância
+resource "aws_volume_attachment" "ebs-volume-1-attach" {
+  device_name = "/dev/xvdh"
+  volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
+  instance_id = "${aws_instance.example.id}"
+}
