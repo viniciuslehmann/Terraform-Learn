5278f93bfcf084e0f86f691d3ac215c96f3015c2
Vinicius Lehmann <viniciuslehmann@gmail.com>
2022-09-28 11:21:50 -0300

    Classic Load Balancer

--- terraform-aws/VPC and NAT/securitygroup.tf
+++ terraform-aws/VPC and NAT/securitygroup.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "allow-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.elb-securitygroup.id]
+  }
+
   tags = {
     SecGroup-Name = "allow-ssh"
   }
@@ -46,4 +53,27 @@ resource "aws_security_group" "allow-mariadb" {
     "Name" = "allow-mariadb"
   }
 
+}
+
+resource "aws_security_group" "elb-securitygroup" {
+  vpc_id      = aws_vpc.main-vpc.id
+  name        = "elb"
+  description = "elb-securitygroup"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "elb"
+  }
 }
